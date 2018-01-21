7fc557a35ade5f9161e0cb05f354a65edaf31639
Juan Gonzalez <jrg2156@gmail.com>
2018-01-21 14:44:38 -0500

    Use sh language for zsh files

--- src-hilite-lesspipe.sh
+++ src-hilite-lesspipe.sh
@@ -3,11 +3,14 @@
 for source in "$@"; do
     case $source in
 	*ChangeLog|*changelog)
-        source-highlight --failsafe -f esc --lang-def=changelog.lang --style-file=esc.style -i "$source" ;;
+            source-highlight --failsafe -f esc --lang-def=changelog.lang --style-file=esc.style -i "$source" ;;
 	*Makefile|*makefile)
-        source-highlight --failsafe -f esc --lang-def=makefile.lang --style-file=esc.style -i "$source" ;;
+            source-highlight --failsafe -f esc --lang-def=makefile.lang --style-file=esc.style -i "$source" ;;
 	*.tar|*.tgz|*.gz|*.bz2|*.xz)
-        lesspipe "$source" ;;
-        *) source-highlight --failsafe --infer-lang -f esc --style-file=esc.style -i "$source" ;;
+            lesspipe "$source" ;;
+        *.zsh)  # HACK: source-highlight doesn't seem to work with zsh; use bash instead
+            source-highlight --failsafe -f esc --lang-def=sh.lang --style-file=esc.style -i "$source" ;
+        *)
+            source-highlight --failsafe --infer-lang -f esc --style-file=esc.style -i "$source" ;;
     esac
 done
