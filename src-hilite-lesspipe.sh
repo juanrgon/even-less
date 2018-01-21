fab4f897441305c3b509361c8f7c1831a85dacb2
Juan Gonzalez <jrg2156@gmail.com>
2018-01-21 14:45:47 -0500

    Fix typo ; => ;;

--- src-hilite-lesspipe.sh
+++ src-hilite-lesspipe.sh
@@ -9,7 +9,7 @@ for source in "$@"; do
 	*.tar|*.tgz|*.gz|*.bz2|*.xz)
             lesspipe "$source" ;;
         *.zsh)  # HACK: source-highlight doesn't seem to work with zsh; use bash instead
-            source-highlight --failsafe -f esc --lang-def=sh.lang --style-file=esc.style -i "$source" ;
+            source-highlight --failsafe -f esc --lang-def=sh.lang --style-file=esc.style -i "$source" ;;
         *)
             source-highlight --failsafe --infer-lang -f esc --style-file=esc.style -i "$source" ;;
     esac
