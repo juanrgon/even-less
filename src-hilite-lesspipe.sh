0b9c7199528ca0c45d2c24295eb270ebe3d77174
Juan Gonzalez <jrg2156@gmail.com>
2018-03-26 14:46:44 -0400

    Be more correct in comment

--- src-hilite-lesspipe.sh
+++ src-hilite-lesspipe.sh
@@ -8,7 +8,7 @@ for source in "$@"; do
             source-highlight --failsafe -f esc --lang-def=makefile.lang --style-file=esc.style -i "$source" ;;
 	*.tar|*.tgz|*.gz|*.bz2|*.xz)
             lesspipe "$source" ;;
-    *.zsh|*.zshrc)  # HACK: source-highlight doesn't seem to work with zsh; use bash instead
+    *.zsh|*.zshrc)  # HACK: source-highlight doesn't seem to work with zsh; use sh instead
         source-highlight --failsafe -f esc --lang-def=sh.lang --style-file=esc.style -i "$source" ;;
     *)
         source-highlight --failsafe --infer-lang -f esc --style-file=esc.style -i "$source" ;;
